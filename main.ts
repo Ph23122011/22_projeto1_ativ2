5a087263e19fcdd4d186cf5fed477972f7cecbe4
Pedro Henrique Silva Conceição <130104477+Ph23122011@users.noreply.github.com>
2023-05-30 13:36:44 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showIcon(IconNames.Sad)
+    basic.pause(500)
+})
